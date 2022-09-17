6cdd845e91da597254789d1a59728edc9ae968b7
Frederick Wijayadi Susilo <frederick.wijayadisusilo@gmail.com>
2022-09-17 17:15:58 +0700

    Delete 704-binary-search directory

--- 704-binary-search/704-binary-search.go
+++
@@ -1,9 +0,0 @@
-func search(nums []int, target int) int {
-    ind := sort.Search(len(nums), func(i int) bool {
-        return nums[i] >= target
-    })
-    if ind < len(nums) && nums[ind] == target {
-        return ind
-    }
-    return -1
-}
