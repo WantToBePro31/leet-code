5a3580d7108315f4c6f7321ce8a502a3024fcb6c
Frederick Wijayadi Susilo <frederick.wijayadisusilo@gmail.com>
2022-09-17 17:34:28 +0700

    Time: 0 ms (100.00%), Space: 1.9 MB (88.10%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.go
@@ -0,0 +1,20 @@
+/** 
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad 
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+    start, end := 1, n
+    for start < end {
+        mid := start + (end-start)/2
+        if isBadVersion(mid) {
+            end = mid
+        } else {
+            start = mid+1
+        }
+    }
+    return start
+}
