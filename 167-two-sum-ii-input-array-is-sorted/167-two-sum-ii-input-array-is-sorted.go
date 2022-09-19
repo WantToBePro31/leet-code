c4aaa9b8609773a69b2d818bfde756c22c60b457
Frederick Wijayadi Susilo <frederick.wijayadisusilo@gmail.com>
2022-09-19 10:28:54 +0700

    Time: 11 ms (92.01%), Space: 5.3 MB (95.30%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
@@ -1,9 +1,11 @@
 func twoSum(numbers []int, target int) []int {
     for ind, val := range numbers {
-        for i := ind+1; i < len(numbers); i++ {
-            if val + numbers[i] == target {
-                return []int{ind+1, i+1}
-            }
+        tmp := target-val
+        key := sort.Search(len(numbers), func(i int) bool {
+            return numbers[i] >= tmp && i != ind
+        })
+        if key < len(numbers) && numbers[key] == tmp && ind != key {
+            return []int{ind+1, key+1}
         }
     }
     return []int{-1,-1}
