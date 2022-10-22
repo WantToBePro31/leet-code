50e89a765fdbe33533e23d2e771892fb63d41ea1
Frederick Wijayadi Susilo <frederick.wijayadisusilo@gmail.com>
2022-10-22 15:28:32 +0700

    Time: 221 ms (59.86%), Space: 13.4 MB (54.42%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
@@ -7,24 +7,15 @@ func abs(a, b int) int {
 
 func containsNearbyDuplicate(nums []int, k int) bool {
     mp1 := make(map[int]int)
-    mp2 := make(map[int][]int)
     for ind, val := range nums {
-        mp1[val]++
-        if _, ok := mp2[val]; !ok {
-            mp2[val] = append(mp2[val],ind)
+        if _, ok := mp1[val]; !ok {
+            mp1[val] = ind
         } else {
-            if len(mp2[val]) != 2 {
-                if abs(mp2[val][0], ind) <= k {
-                    mp2[val] = append(mp2[val],ind)
-                } else {
-                    mp2[val][0] = ind
-                }
+            if abs(mp1[val], ind) <= k {
+                return true
+            } else {
+                mp1[val] = ind
             }
-        }
-    }
-    for key, val := range mp1 {
-        if val > 1 && len(mp2[key]) == 2 {
-            return true
         }
     }
     return false
